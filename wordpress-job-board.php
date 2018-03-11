4163e3de6a5584c0536cc2dcc39901865f43b4dd
thephpx <thephpx@gmail.com>
2018-03-10 21:22:23 -0500

    Added automated test config and readme file

--- wordpress-job-board.php
+++ wordpress-job-board.php
@@ -1,2 +1,10 @@
+<?php
 /*
+Plugin Name: WordPress Job Board
+Plugin URI: https://thephpx.wordpress.org/plugins/wordpress-job-board/
+Description: Free wordpress job board
+Version: 0.1.0
+Author: Faisal Ahmed
+Author URI: http://www.faisalbd.com
+Text Domain: wordpress-job-board
 */
